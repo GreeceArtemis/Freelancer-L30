2de22a10fc13db44100128347069abc74f9dec84
GreeceArtemis <greece.rapu@gmail.com>
2017-05-09 18:51:11 -0500

    v2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 /*
  * Archivo principal de JS
  */
+window.addEventListener("scroll",function(){
+var scroll= window.pageYOffset || document.documentElement.scrollTop;
+var =document.getElementsByClassName("navt")
+var header=document.getElementsByClassName("title-type")[0];
+if(scroll<=90){
+  header.style.height="100px";
+
+}
+else{
+  header.style.height="70px";
+}
+})
