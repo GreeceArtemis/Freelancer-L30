dc7b6255d3a5657e251cc2367887a8e407e3fd3a
GreeceArtemis <greece.rapu@gmail.com>
2017-05-10 14:14:03 -0500

    html y css terminado

--- js/app.js
+++ js/app.js
@@ -16,4 +16,32 @@ else{
   titleStart.classList.add("startSmall");
   titleStart.classList.remove("startBig");
 }
+if(scroll>677&&scroll<1584){
+  document.getElementsByClassName("portfolio")[0].style.background="#18BC9C";
+}
+else{
+  document.getElementsByClassName("portfolio")[0].style.background="#2C3E50";
+}
+if(scroll>=1584&&scroll<2125){
+  document.getElementsByClassName("about")[0].style.background="#18BC9C";
+}
+else{
+  document.getElementsByClassName("about")[0].style.background="#2C3E50";
+}
+if(scroll>=2125){
+  document.getElementsByClassName("contact")[0].style.background="#18BC9C";
+}
+else{
+  document.getElementsByClassName("contact")[0].style.background="#2C3E50";
+}
 })
+
+var images=document.getElementsByClassName("img");
+for(var i=0;i<images.length;i++){
+  var div=document.createElement("div");
+  span.setAttribute("class","search");
+  var lupa=document.createElement("span");
+  lupa.setAttribute("class","fa fa-search-plus");
+  div.appendChild(lupa);
+  images[i].appendChild(div);
+}
