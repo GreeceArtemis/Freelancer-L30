c6b069f17cc2ee769d074f71e90a12afd947461f
GreeceArtemis <greece.rapu@gmail.com>
2017-05-10 00:55:29 -0500

    v2

--- js/app.js
+++ js/app.js
@@ -3,13 +3,17 @@
  */
 window.addEventListener("scroll",function(){
 var scroll= window.pageYOffset || document.documentElement.scrollTop;
-var =document.getElementsByClassName("navt")
+var titleStart=document.getElementsByClassName("title-start")[0];
 var header=document.getElementsByClassName("title-type")[0];
 if(scroll<=90){
   header.style.height="100px";
+  titleStart.classList.add("startBig");
+  titleStart.classList.remove("startSmall");
 
 }
 else{
   header.style.height="70px";
+  titleStart.classList.add("startSmall");
+  titleStart.classList.remove("startBig");
 }
 })
